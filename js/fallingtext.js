9a68e7589320cddeb99607ab2a3f528a1564f875
Doctor Wraith <thomas.vansteen2004@gmail.com>
2025-09-26 18:26:35 +0200

    Pretty buttons

---
+++ js/fallingtext.js
@@ -0,0 +1,39 @@
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let cols = Math.floor(window.innerWidth / 20) + 1;
+let ypos = Array(cols).fill(0);
+
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function matrix () {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  
+  if (canvas.width !== w) {
+    canvas.width = w;
+    cols = Math.floor(window.innerWidth / 20) + 1;
+    ypos = Array(cols).fill(0);
+  }
+  if (canvas.height !== h) {
+    canvas.height = h;
+  }
+
+  ctx.fillStyle = '#0001';
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.fillStyle = '#0f0';
+  ctx.font = '15pt monospace';
+
+  ypos.forEach((y, ind) => {
+    const text = String.fromCharCode(Math.random() * 128);
+    const x = ind * 20;
+    ctx.fillText(text, x, y);
+    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+    else ypos[ind] = y + 20;
+  });
+}
+
+setInterval(matrix, 50);
